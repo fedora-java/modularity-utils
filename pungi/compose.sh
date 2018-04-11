e008456bff0c1937905ca63d77249740137b3d7c
Mikolaj Izdebski <mizdebsk@redhat.com>
2018-04-11 19:49:29 +0200

    Layer our buildroot on top of regular buildroot

--- pungi/compose.sh
+++ pungi/compose.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
+set -e
 
-exec pungi-koji --no-label --target-dir /mnt/koji/compose/mizdebsk --config pungi.conf
+pungi-koji --no-label --target-dir /mnt/koji/compose/mizdebsk --config pungi.conf
+
+# Layer our buildroot on top of regular buildroot
+rm -rf merged_repo
+mergerepo_c \
+    --repo file:///mnt/koji/compose/mizdebsk/latest-Fedora-Java/compose/Buildroot/x86_64/os/ \
+    --repo file:///mnt/fedora_koji_prod/koji/repos/f29-build/latest/x86_64/
+rm -rf /mnt/koji/compose/mizdebsk/hybrid-buildroot
+mv merged_repo /mnt/koji/compose/mizdebsk/hybrid-buildroot
